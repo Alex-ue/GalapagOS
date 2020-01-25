c5bb1eeba57f8729e029e5fe1f85543b468e17e4
Alex <58010932+Alex-ue@users.noreply.github.com>
2020-01-25 16:40:48 +0000

    Added table

--- src/pages/overview.py
+++ src/pages/overview.py
@@ -1,5 +1,11 @@
 from app import app, html, dcc, dash
 
+#vars for table
+global sensors
+global values
+sensors = ["S1","S2","S3","S4","S5"]
+values = [1,2,3,4,5]
+
 overview = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(
@@ -8,7 +14,16 @@ overview = html.Div([
                  for i in ['LA', 'NYC', 'MTL']],
         value='LA'
     ),
-    html.Div(id='display-value')
+    html.Div(id='display-value'),
+	
+	html.Table(
+                
+		#Sensor names
+		[html.Tr([html.Th("Sensor",), html.Th("Value")])] +
+		
+		#Sensor Values
+		[html.Tr([html.Td(sensors[i]), html.Td(values[i])]) for i in range(max(len(sensors),len(values)))]
+		)
 ])
 
 
@@ -18,3 +33,4 @@ overview = html.Div([
 )
 def display_value(value):
     return 'You have selected "{}"'.format(value)
+	
